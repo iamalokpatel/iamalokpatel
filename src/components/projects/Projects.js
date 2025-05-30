47c3bd604b9f53e632cb2c07ff795928cc9d173a
Alok Patel <alokpatel23273@gmail.com>
2025-05-30 11:49:02 +0530

    commited

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Title from "../layouts/Title";
 import ProjectsCard from "./ProjectsCard";
-import Image from "next/image";
 
 const Projects = () => {
   return (
@@ -14,26 +13,26 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
+          title="Ecozon"
+          des=" Built an E-Commerce website using Node.js, Express.js, Next.js, Taiwind CSS, MongoDB, and Cloudinary."
+          src="/images/projects/projectTwo.png"
+          gitLink="https://github.com/iamalokpatel/Ecozon.git"
+          liveLink="https://ecozon.vercel.app/"
+        />
+        <ProjectsCard
           title="Airbnb Clone"
           des=" Built an Airbnb Clone using EJS, Node.js, Express.js, and
                 MongoDB with user auth, and property listings."
           src="/images/projects/projectOne.png"
-          gitLink="https://github.com/iamalokpatel/ZerodhaUi"
-          liveLink="https://iamalokpatel.github.io/ZerodhaUi/"
+          gitLink="https://github.com/iamalokpatel/AirbnbClone.git"
+          liveLink="https://airbnbclone-791g.onrender.com/listings"
         />
+
         <ProjectsCard
           title="Spotify UI"
           des=" Built an Spotify UI  using HTML CSS, and
                 JavaScript with listings, Property Booking System cards."
           src="/images/projects/projectThree.png"
-          gitLink="https://github.com/iamalokpatel"
-          liveLink="https://github.com/iamalokpatel"
-        />
-        <ProjectsCard
-          title="AssignMent"
-          des=" Built an marketing UI  using Next.js, Taiwind CSS, and
-                JavaScript with Resposiveness."
-          src="/images/projects/projectTwo.png"
           gitLink="https://github.com/iamalokpatel"
           liveLink="https://github.com/iamalokpatel"
         />
